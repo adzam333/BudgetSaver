2c03e221060ac382901dd56484406c66160a2ceb
adzam333 <adam.kotvas@gmail.com>
2025-06-06 13:22:56 +0200

    Add basic models

---
+++ BudgetSaver/Models/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace BudgetSaver.Models;
+
+public class Category
+{
+        public int Id { get; set; }
+        public string? Name { get; set; }
+}
---
+++ BudgetSaver/Models/SavingsGoal.cs
@@ -0,0 +1,10 @@
+﻿namespace BudgetSaver.Models;
+
+public class SavingsGoal
+{
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal TargetAmount { get; set; }
+        public decimal CurrentAmount { get; set; }
+        public DateTime? DueDate { get; set; }
+}
