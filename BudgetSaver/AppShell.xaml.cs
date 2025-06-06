0ef2dc90865a0c726268a5208e9d6a9cae8e79e0
adzam333 <adam.kotvas@gmail.com>
2025-06-06 13:15:16 +0200

    Remove font assets

---
+++ BudgetSaver/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BudgetSaver;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BudgetSaver
+
+This repository contains a minimal .NET MAUI project generated with `dotnet new maui`.
